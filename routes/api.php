401b02b902929d2f1340b9bb245d87cbef5d4667
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-12-15 18:10:05 +0700

    Edit: perbaikan route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ChatController;
 use App\Http\Controllers\LikedController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PaymentController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
